846b654151c20802ac5ecf0028925ac39051b35e
Amel Musić <amel.music@kodecta.com>
2022-05-23 21:04:32 +0200

    V10

---
+++ eprodajamobile/android/app/src/main/kotlin/com/example/eprodajamobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eprodajamobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ eprodajamobile/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Row Example"),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/background.png"),
+                    fit: BoxFit.fill)),
+            child: Stack(children: [
+              Positioned(
+                  left: 120,
+                  top: 0,
+                  width: 80,
+                  height: 120,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                    image: AssetImage("assets/images/light-1.png"),
+                  )))),
+              Positioned(
+                  right: 40,
+                  top: 0,
+                  width: 80,
+                  height: 120,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                    image: AssetImage("assets/images/clock.png"),
+                  )))),
+              Container(
+                child: Center(
+                  child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)
+                  ),
+              )
+            ]),
+          ),
+          Padding(
+            padding: EdgeInsets.all(40),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey))
+                  ),
+                  child: TextField(
+                          decoration: InputDecoration(border: InputBorder.none, hintText: "Email or phone", hintStyle: TextStyle(color: Colors.grey[400])),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: TextField(
+                          decoration: InputDecoration(border: InputBorder.none, hintText: "Pasword", hintStyle: TextStyle(color: Colors.grey[400])),
+                  ),
+                ),
+
+                
+              ]),
+            ),
+          ),
+          SizedBox(height: 2,),
+          Container(
+                  height: 50,
+                  margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      colors: [Color.fromRGBO(143, 148, 251, 1), Color.fromRGBO(143, 148, 251, .6)]
+                    ),
+                    
+                  ),
+                  child: Center(child: Text("Login")),
+                ),
+          SizedBox(height: 40,),
+          Text("Forgot password?"),
+          SizedBox(height: 40,),
+        ],
+      ),
+      ),
+    );
+  }
+}
